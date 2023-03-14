6f0e1ed67d0a39a4d11a2a8ca7232fab162c7353
LexLuter <gae_cbr@mail.ru>
2023-03-14 23:02:11 +0700

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int M, int N)
+{
+    if (M == N) return M;
+    return (M + SumNumbers(M + 1, N));
+}
+Console.WriteLine(SumNumbers(M, N));
+
+
