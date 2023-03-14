f7657a122a28bd9d2bae4476198344d2d609ad5a
LexLuter <gae_cbr@mail.ru>
2023-03-14 22:53:12 +0700

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbers(int end, int start)
+ {
+     
+    if (start == end) return start.ToString();
+    return (end + ", " + PrintNumbers(end-1, start));
+ }
+
+ Console.WriteLine(PrintNumbers(N, 1));
