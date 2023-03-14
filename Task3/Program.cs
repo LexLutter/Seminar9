d849c57cebf4f5eb7d5898fc49d38eb3afb67f95
LexLuter <gae_cbr@mail.ru>
2023-03-14 23:15:25 +0700

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman(int N, int M)
+{
+    if (N == 0) return M + 1;
+    else if (N != 0 && M == 0) return (Akkerman(N - 1, 1));
+    else return (Akkerman(N - 1, Akkerman(N, M - 1)));
+}
+Console.WriteLine(Akkerman(N, M));
+
+
